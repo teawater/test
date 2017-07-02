e10e0ae3fe258d69cacaf0f5fa9ae5a3215a0acb
teawater <teawater@126.com>
2017-07-03 00:00:04 +0800

    Update

--- http.go
+++ http.go
@@ -9,7 +9,7 @@ const ssl_crt = "../server.crt"
 const ssl_key = "../server.key"
 
 func main() {
-	http.Handle("/w", http.FileServer(http.Dir("webroot")))
+	http.Handle("/", http.FileServer(http.Dir("./")))
 
 	log.Fatal(http.ListenAndServeTLS(":443", ssl_crt, ssl_key, nil))
 	//log.Fatal(http.ListenAndServe(":80", nil))
