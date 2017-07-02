d881877e4531141a5f54a2f4631de82d73cc5735
teawater <teawater@126.com>
2017-07-03 00:21:04 +0800

    Update

--- http.go
+++ http.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"crypto/tls"
 )
 
 const ssl_crt = "../server.crt"
@@ -11,6 +12,11 @@ const ssl_key = "../server.key"
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./")))
 
+	config := &tls.Config{MinVersion: tls.VersionTLS10}
+	server := &http.Server{Addr: ":443", Handler: nil, TLSConfig: config}
+	log.Fatal(server.ListenAndServeTLS(ssl_crt, ssl_key))
+
 	log.Fatal(http.ListenAndServeTLS(":443", ssl_crt, ssl_key, nil))
+
 	//log.Fatal(http.ListenAndServe(":80", nil))
 }
